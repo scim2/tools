e1c873dadbf26ec62865440c49eaf76dc9e0549e
Quint <me@di-wu.be>
2020-10-03 18:53:50 +0200

    Add core attributes to generator.

--- structs/generate/struct.go
+++ structs/generate/struct.go
@@ -1,113 +1,135 @@
 package gen
 
 import (
-	"bytes"
-	"errors"
-	"github.com/scim2/tools/schema"
-	"strings"
+    "bytes"
+    "errors"
+    "github.com/scim2/tools/schema"
+    "sort"
+    "strings"
 )
 
 type StructGenerator struct {
-	w   *genWriter
-	s   schema.ReferenceSchema
-	ptr bool
+    w   *genWriter
+    s   schema.ReferenceSchema
+    ptr bool
 }
 
 func NewStructGenerator(s schema.ReferenceSchema) (StructGenerator, error) {
-	if s.Name == "" {
-		return StructGenerator{}, errors.New("schema does not have a name")
-	}
+    if s.Name == "" {
+        return StructGenerator{}, errors.New("schema does not have a name")
+    }
 
-	return StructGenerator{
-		w: newGenWriter(&bytes.Buffer{}),
-		s: s,
-	}, nil
+    // check if id an externalId is present, add if not
+    var id, eid bool
+    for _, a := range s.Attributes {
+        if strings.ToLower(a.Name) == "id" {
+            id = true
+        }
+        if strings.ToLower(a.Name) == "externalid" {
+            eid = true
+        }
+    }
+    if !id {
+        s.Attributes = append(s.Attributes, schema.IDAttribute)
+    }
+    if !eid {
+        s.Attributes = append(s.Attributes, schema.ExternalIDAttribute)
+    }
+
+    sort.SliceStable(s.Attributes, func(i, j int) bool {
+        return strings.ToLower(s.Attributes[i].Name) < strings.ToLower(s.Attributes[j].Name)
+    })
+
+    return StructGenerator{
+        w: newGenWriter(&bytes.Buffer{}),
+        s: s,
+    }, nil
 }
 
 // UsePtr indicates whether the generator will use pointers if the attribute is not required.
 func (g *StructGenerator) UsePtr(t bool) *StructGenerator {
-	g.ptr = t
-	return g
+    g.ptr = t
+    return g
 }
 
 // Generate creates a buffer with a go representation of the resource described in the given schema.
 func (g *StructGenerator) Generate() *bytes.Buffer {
-	g.generateStruct(g.s.Name, g.s.Description, g.s.Attributes)
-	return g.w.writer.(*bytes.Buffer)
+    g.generateStruct(g.s.Name, g.s.Description, g.s.Attributes)
+    return g.w.writer.(*bytes.Buffer)
 }
 
 func (g *StructGenerator) generateStruct(name, desc string, attrs []*schema.Attribute) {
-	w := g.w
+    w := g.w
 
-	name = keepAlpha(name) // remove all non alpha characters
+    name = keepAlpha(name) // remove all non alpha characters
 
-	if desc != "" {
-		w.ln(comment(wrap(desc, 117))) // 120 - "// "
-	}
+    if desc != "" {
+        w.ln(comment(wrap(desc, 117))) // 120 - "// "
+    }
 
-	if len(attrs) == 0 {
-		w.lnf("type %s struct {}", name)
-		return
-	}
+    if len(attrs) == 0 {
+        w.lnf("type %s struct {}", name)
+        return
+    }
 
-	w.lnf("type %s struct {", name)
-	g.generateStructFields(name, attrs)
-	w.ln("}")
+    w.lnf("type %s struct {", name)
+    g.generateStructFields(name, attrs)
+    w.ln("}")
 
-	for _, attr := range attrs {
-		if attr.Type == schema.ComplexType {
-			typ := cap(attr.Name)
-			if attr.MultiValued && strings.HasSuffix(typ, "s") {
-				typ = strings.TrimSuffix(typ, "s")
-			}
-			w.n()
-			g.generateStruct(name+typ, attr.Description, attr.SubAttributes)
-		}
-	}
+    for _, attr := range attrs {
+        if attr.Type == schema.ComplexType {
+            typ := cap(attr.Name)
+            if attr.MultiValued && strings.HasSuffix(typ, "s") {
+                typ = strings.TrimSuffix(typ, "s")
+            }
+            w.n()
+            g.generateStruct(name+typ, attr.Description, attr.SubAttributes)
+        }
+    }
 }
 
 func (g *StructGenerator) generateStructFields(name string, attrs []*schema.Attribute) {
-	w := g.w
+    w := g.w
 
-	name = keepAlpha(name) // remove all non alpha characters
+    name = keepAlpha(name) // remove all non alpha characters
 
-	// get longest name to indent fields.
-	var indent int
-	for _, attr := range attrs {
-		if l := len(cap(attr.Name)); l > indent {
-			indent = l
-		}
-	}
+    // get longest name to indent fields.
+    var indent int
+    for _, attr := range attrs {
+        if l := len(cap(attr.Name)); l > indent {
+            indent = l
+        }
+    }
 
-	for _, attr := range attrs {
-		var typ string
-		switch t := attr.Type; t {
-		case "decimal":
-			typ = "float64"
-		case "integer":
-			typ = "int"
-		case "boolean":
-			typ = "bool"
-		case "complex":
-			typ = cap(name + cap(attr.Name))
-		default:
-			typ = "string"
-		}
+    for _, attr := range attrs {
+        var typ string
+        switch t := attr.Type; t {
+        case "decimal":
+            typ = "float64"
+        case "integer":
+            typ = "int"
+        case "boolean":
+            typ = "bool"
+        case "complex":
+            typ = cap(name + cap(attr.Name))
+        default:
+            typ = "string"
+        }
 
-		// field name
-		name := cap(keepAlpha(attr.Name))
-		w.in(4).w(name)
-		w.sp(indent - len(name) + 1)
+        // field name
+        name := cap(keepAlpha(attr.Name))
+        w.in(4).w(name)
+        w.sp(indent - len(name) + 1)
 
-		if attr.MultiValued {
-			w.w("[]")
-			if strings.HasSuffix(typ, "s") {
-				typ = strings.TrimSuffix(typ, "s")
-			}
-		} else if !attr.Required && g.ptr {
-			w.w("*")
-		}
+        if attr.MultiValued {
+            w.w("[]")
+            if strings.HasSuffix(typ, "s") {
+                typ = strings.TrimSuffix(typ, "s")
+            }
+        } else if !attr.Required && g.ptr {
+            w.w("*")
+        }
 
-		w.ln(typ)
-	}
+        w.ln(typ)
+    }
 }
--- structs/generate/struct_test.go
+++ structs/generate/struct_test.go
@@ -23,7 +23,10 @@ func ExampleStructGenerator_Generate_empty() {
 
 	// Output:
 	// // User Account
-	// type User struct {}
+	// type User struct {
+	//     ExternalId string
+	//     Id         string
+	// }
 }
 
 func ExampleStructGenerator_Generate_minimal() {
@@ -41,7 +44,9 @@ func ExampleStructGenerator_Generate_minimal() {
 
 	// Output:
 	// type User struct {
-	//     UserName string
+	//     ExternalId string
+	//     Id         string
+	//     UserName   string
 	// }
 }
 
@@ -56,29 +61,76 @@ func ExampleStructGenerator_Generate_user() {
 	// Output:
 	// // User Account
 	// type User struct {
-	//     UserName          string
-	//     Name              *UserName
-	//     DisplayName       *string
-	//     NickName          *string
-	//     ProfileUrl        *string
-	//     Title             *string
-	//     UserType          *string
-	//     PreferredLanguage *string
-	//     Locale            *string
-	//     Timezone          *string
 	//     Active            *bool
-	//     Password          *string
-	//     Emails            []UserEmail
-	//     PhoneNumbers      []UserPhoneNumber
-	//     Ims               []UserIm
-	//     Photos            []UserPhoto
 	//     Addresses         []UserAddresse
-	//     Groups            []UserGroup
+	//     DisplayName       *string
+	//     Emails            []UserEmail
 	//     Entitlements      []UserEntitlement
+	//     ExternalId        *string
+	//     Groups            []UserGroup
+	//     Id                string
+	//     Ims               []UserIm
+	//     Locale            *string
+	//     Name              *UserName
+	//     NickName          *string
+	//     Password          *string
+	//     PhoneNumbers      []UserPhoneNumber
+	//     Photos            []UserPhoto
+	//     PreferredLanguage *string
+	//     ProfileUrl        *string
 	//     Roles             []UserRole
+	//     Timezone          *string
+	//     Title             *string
+	//     UserName          string
+	//     UserType          *string
 	//     X509Certificates  []UserX509Certificate
 	// }
-	//
+	// 
+	// // A physical mailing address for this User. Canonical type values of 'work', 'home', and 'other'. This attribute is a
+	// // type with the following sub-attributes.
+	// type UserAddresse struct {
+	//     Formatted     *string
+	//     StreetAddress *string
+	//     Locality      *string
+	//     Region        *string
+	//     PostalCode    *string
+	//     Country       *string
+	//     Type          *string
+	// }
+	// 
+	// // Email addresses for the user. The value SHOULD be canonicalized by the service provider, e.g., 'bjensen@example.com'
+	// // of 'bjensen@EXAMPLE.COM'. Canonical type values of 'work', 'home', and 'other'.
+	// type UserEmail struct {
+	//     Value   *string
+	//     Display *string
+	//     Type    *string
+	//     Primary *bool
+	// }
+	// 
+	// // A list of entitlements for the User that represent a thing the User has.
+	// type UserEntitlement struct {
+	//     Value   *string
+	//     Display *string
+	//     Type    *string
+	//     Primary *bool
+	// }
+	// 
+	// // A list of groups to which the user belongs, either through direct membership, through nested groups, or dynamically
+	// type UserGroup struct {
+	//     Value   *string
+	//     Ref     *string
+	//     Display *string
+	//     Type    *string
+	// }
+	// 
+	// // Instant messaging addresses for the User.
+	// type UserIm struct {
+	//     Value   *string
+	//     Display *string
+	//     Type    *string
+	//     Primary *bool
+	// }
+	// 
 	// // The components of the user's real name. Providers MAY return just the full name as a single string in the formatted
 	// // or they MAY return just the individual component attributes using the other sub-attributes, or they MAY return both.
 	// // both variants are returned, they SHOULD be describing the same name, with the formatted name indicating how the
@@ -91,16 +143,7 @@ func ExampleStructGenerator_Generate_user() {
 	//     HonorificPrefix *string
 	//     HonorificSuffix *string
 	// }
-	//
-	// // Email addresses for the user. The value SHOULD be canonicalized by the service provider, e.g., 'bjensen@example.com'
-	// // of 'bjensen@EXAMPLE.COM'. Canonical type values of 'work', 'home', and 'other'.
-	// type UserEmail struct {
-	//     Value   *string
-	//     Display *string
-	//     Type    *string
-	//     Primary *bool
-	// }
-	//
+	// 
 	// // Phone numbers for the User. The value SHOULD be canonicalized by the service provider according to the format
 	// // in RFC 3966, e.g., 'tel:+1-201-555-0123'. Canonical type values of 'work', 'home', 'mobile', 'fax', 'pager', and
 	// type UserPhoneNumber struct {
@@ -109,15 +152,7 @@ func ExampleStructGenerator_Generate_user() {
 	//     Type    *string
 	//     Primary *bool
 	// }
-	//
-	// // Instant messaging addresses for the User.
-	// type UserIm struct {
-	//     Value   *string
-	//     Display *string
-	//     Type    *string
-	//     Primary *bool
-	// }
-	//
+	// 
 	// // URLs of photos of the User.
 	// type UserPhoto struct {
 	//     Value   *string
@@ -125,35 +160,7 @@ func ExampleStructGenerator_Generate_user() {
 	//     Type    *string
 	//     Primary *bool
 	// }
-	//
-	// // A physical mailing address for this User. Canonical type values of 'work', 'home', and 'other'. This attribute is a
-	// // type with the following sub-attributes.
-	// type UserAddresse struct {
-	//     Formatted     *string
-	//     StreetAddress *string
-	//     Locality      *string
-	//     Region        *string
-	//     PostalCode    *string
-	//     Country       *string
-	//     Type          *string
-	// }
-	//
-	// // A list of groups to which the user belongs, either through direct membership, through nested groups, or dynamically
-	// type UserGroup struct {
-	//     Value   *string
-	//     Ref     *string
-	//     Display *string
-	//     Type    *string
-	// }
-	//
-	// // A list of entitlements for the User that represent a thing the User has.
-	// type UserEntitlement struct {
-	//     Value   *string
-	//     Display *string
-	//     Type    *string
-	//     Primary *bool
-	// }
-	//
+	// 
 	// // A list of roles for the User that collectively represent who the User is, e.g., 'Student', 'Faculty'.
 	// type UserRole struct {
 	//     Value   *string
@@ -161,7 +168,7 @@ func ExampleStructGenerator_Generate_user() {
 	//     Type    *string
 	//     Primary *bool
 	// }
-	//
+	// 
 	// // A list of certificates issued to the User.
 	// type UserX509Certificate struct {
 	//     Value   *string
